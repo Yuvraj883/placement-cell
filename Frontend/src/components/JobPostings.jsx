cd78d379a052fffe13b2a185c19c50d08991dc2c
Yuvraj <devyuvraj883@gmail.com>
2024-09-13 19:40:01 +0530

    Added option chaining

--- Frontend/src/components/JobPostings.jsx
+++ Frontend/src/components/JobPostings.jsx
@@ -41,7 +41,7 @@ const JobPostings = () => {
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {jobPostings.map((job) => (
-            <JobCard key={job._id} job={job} /> // Use job._id as key
+            <JobCard key={job?._id} job={job} /> // Use job._id as key
           ))}
         </div>
       )}
